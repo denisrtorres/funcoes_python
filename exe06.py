ecf669cc395ca23553dad462e9eff231a66b4025
Denis <denisrtorres18@hotmail.com>
2026-05-27 21:15:14 -0300

    Contar vogais

---
+++ exe06.py
@@ -0,0 +1,9 @@
+def contar_vogais(texto):
+    count_vogal = 0
+    for letra in texto:
+        if letra in "AEIOU":
+            count_vogal +=1
+    
+    return count_vogal
+
+print(contar_vogais("DENIS"))
